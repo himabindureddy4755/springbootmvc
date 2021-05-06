a2111206aa0b09f7b0afdc8c46ad0e202d5f9492
hp <bindureddy4755@gmail.com>
2021-05-06 13:06:45 +0530

    developers and their skills

---
+++ springbootmvc/src/main/java/com/example/demo/repository/DeveloperRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Developer;
+public interface DeveloperRepository extends CrudRepository<Developer, Long> {
+
+	
+
+
+
+}
